c8ac1b81310bd97113346512cd99fbb0a68ba4ff
Cleverlike <cleverlike@users.noreply.github.com>
2026-02-14 22:18:17 -0700

    Fix base path configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    base: '/SpecialAgentName/',
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/SpecialAgentName/',
       server: {
         port: 3000,
         host: '0.0.0.0',
